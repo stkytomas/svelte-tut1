8e37d7acb2a7a0ee492824ba3c86f6cab3563037
Savannah Tomas <savannah@lelanders-MBP.core10-office.com>
2021-04-09 11:47:24 -0500

    added Player and Navbar files

--- src/App.svelte
+++ src/App.svelte
@@ -3,19 +3,28 @@
 
 	let name = "Tony Stark";
 	let points = 100;
+	let showControls = false;
 
 const addPoint = () => points += 1;
 const removePoint = () => points -= 1;
+const toggleControls = () => (showControls = !showControls);
 </script>
 
 <main>
 	<div class="container">
 		<div class="card">
-			<h1>{name}</h1>
-			<h3>{points}</h3>
-			<button class="btn" on:click={addPoint}>+1</button>
-			<button class="btn btn-2" on:click={removePoint}>-1</button>
-			<input type="number" bind:value={points} />
+			<h1>
+				{name}
+				<button class="btn btn-sm" on:click={toggleControls}>
+				{#if showControls}-{:else}+{/if}
+				</button>
+			</h1>
+			<h3>Current Points: {points}</h3>
+			{#if showControls}
+				<button class="btn" on:click={addPoint}>+1</button>
+				<button class="btn btn-2" on:click={removePoint}>-1</button>
+				<input type="number" bind:value={points} />
+			{/if}
 		</div>
 	</div>	
 </main>
